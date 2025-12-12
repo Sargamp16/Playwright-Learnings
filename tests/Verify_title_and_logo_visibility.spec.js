efc6686e37549ddcede27d7552bae298823494df
Sargam Panwar <sargampanwar@COAD-PH39YDW76X.local>
2025-12-12 11:48:45 +0530

    Test Cases title and logo

---
+++ tests/Verify_title_and_logo_visibility.spec.js
@@ -0,0 +1,15 @@
+import {test,expect} from '@playwright/test';
+
+test("Title validation", async ({ page }) =>
+{
+    await page.goto("https://automationexercise.com/");
+    const Title = await page.title();
+    expect(Title).toBe("Automation Exercise");
+})
+
+test("Logo visibility" , async({page})=>
+{
+    await page.goto("https://automationexercise.com/");
+    const logo=await page.locator('.logo').isVisible();
+    expect(logo).toBeTruthy();
+});
