c40c200bb156b60564deab5d1a2c165d6492c5c6
Sargam Panwar <sargampanwar@COAD-PH39YDW76X.local>
2025-12-12 10:39:17 +0530

    Test Cases for logout functionality

---
+++ tests/Logout.spec.js
@@ -0,0 +1,15 @@
+import{test,expect} from '@playwright/test';
+test("Logout Functionality Test",async({page})=>
+{
+    await page.goto("https://automationexercise.com/");
+    await page.waitForLoadState('networkidle');
+    await page.locator('.fa-lock').click();
+    await page.locator('[data-qa="login-email"]').click();
+    await page.locator('[data-qa="login-email"]').fill('sargam01@yopmail.com');
+    await page.locator('[data-qa="login-password"]').click();
+    await page.locator('[data-qa="login-password"]').fill('Password123');
+    await page.getByRole('button',{name:'Login'}).click();
+    await expect(page.getByText('Logged in as')).toBeVisible();
+    await page.locator('[href*="logout"]').click();
+    await expect(page.getByText('Login to your account')).toBeVisible();
+})
